0047de50e244823deb91e54472b9bd66fd37d841
Benjamin Ejike <benjamin.ejike.work@gmail.com>
2026-09-21 21:39:01 +0100

    mdified the into  page

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './intro.css'
 import bg from '../../assets/image.png';
+import btnImg  from '../../assets/hireme.png'
+import {Link} from 'react-scroll'
 
 const Intro = () => {
   return (
@@ -8,7 +10,9 @@ const Intro = () => {
         <div className="introContent">
             <span className="hello">Hello,</span>
             <span className="introText">I'm <span className="introName">Benjamin</span> <br />Computer Science Student</span>
-
+            <p className="introPara">I am a third-year Computer Science student with experience in 
+                software development.</p>
+            <Link><button className="btn"><img src={btnImg} alt="Hire me" className='btnImg'/>Hire me</button></Link>
         </div>
         <img src={bg} alt="Profile" className="bg" />
     </section>
