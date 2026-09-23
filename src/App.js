986e89217c0a526431023a42a692a34039ce7bbe
Benjamin Ejike <benjamin.ejike.work@gmail.com>
2026-09-23 13:10:14 +0100

    finsihed the footer section and also completed the design layout of the website

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Intro from './components/Intro/intro';
 import Skills from './components/Skills/skills';
 import Works from './components/Works/works'
 import Contact from './components/Contact/contact';
+import Footer from './components/Footer/footer';
 
 function App() {
     return (
@@ -11,7 +12,8 @@ function App() {
             <Intro />
             <Skills /> 
             <Works />
-            <Contact/>
+            <Contact />
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer'
+
+const Footer = () => {
+  return (
+    <footer className= 'footer'>
+    Copyright &#169; 2026 Benjamin Ejike. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
