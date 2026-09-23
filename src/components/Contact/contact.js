f347b44c22d76a7ca87a2df22576c2d2416cfe75
Benjamin Ejike <benjamin.ejike.work@gmail.com>
2026-09-23 17:26:03 +0100

    completed the contact page with email.JS

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import './contact.css';
 import Walmart from '../../assets/walmart.png';
 import Microsoft from '../../assets/microsoft.png';
@@ -8,7 +8,27 @@ import FacebookIcon from '../../assets/facebook-icon.png';
 import YoutubeIcon from '../../assets/youtube.png';
 import TwitterIcon from '../../assets/twitter.png';
 import InstagramIcon from '../../assets/instagram.png';
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+     const form = useRef();
+      const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_n9g4q69', 'template_zjt71jn', form.current, {
+        publicKey: 'AWKtRevFLqqL1aGlT',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          form.current.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <section id="contactPage">
         <div id="client">
@@ -27,9 +47,9 @@ const Contact = () => {
         <div id="contact">
             <h1 className="contactPageTitle">Contact Me</h1>
             <span className="contactDesc">Please fill out the form to discuss any work opportunities</span>
-            <form className='contactForm'>
-                <input type="text" className="name"placeholder='Your Name' />
-                <input type="email" className="email"placeholder='Your Email' />
+            <form className='contactForm' ref ={form} onSubmit={sendEmail}>
+                <input type="text" className="name"placeholder='Your Name' name ='your_name' />
+                <input type="email" className="email"placeholder='Your Email' name='your_email' />
                 <textarea className= 'msg' name="message" rows="5"placeholder='Your Message'></textarea>
                 <button type='submit' value='Send'className="submitBtn">Submit</button>
                 <div className="links">
